f0eb70e76d1b32df6f6b0720ce753b7c7ba859e2
davidliu668 <liuzhuofu1984@163.com>
2016-11-14 20:47:00 +0800

    update admin setting

--- wx_website/apps/expense/admin.py
+++ wx_website/apps/expense/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from models import expense_list
+
 # Register your models here.
+
+
+
+class ExpenseListAdmin(admin.ModelAdmin):
+    list_display = ('time', 'money', 'remark')
+    list_filter = ('time', 'money')
+    search_fields = ('time', 'money', 'remark')
+    ording=('-time',)
+
+admin.site.register(expense_list, ExpenseListAdmin)
